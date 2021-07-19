cad3dfaef88e527b39ceed80a59ad1bdee62e17b
bushuev-alex <bushuev-alex@mail.ru>
2021-07-19 22:49:59 +0700

    Init LoginPage in body of tests

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,6 @@
-from .pages.main_page import MainPage
-from .pages.locators import MainPageLocators, LoginPageLocators
+from .pages.locators import MainPageLocators
 from .pages.login_page import LoginPage
+from .pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -8,6 +8,8 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()  # открываем страницу
     page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 
 def test_guest_should_see_login_link(browser):
